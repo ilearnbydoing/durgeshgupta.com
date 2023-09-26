4d6a312101dd3cae347a95ef4c1e9e979543288c
ilearnbydoing <dgweb123@gmail.com>
2023-09-26 11:24:43 +0530

    content update

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -1,22 +1,23 @@
 // please update required information
 const siteMetadata = {
-    title: 'Next.js Blog With Tailwind CSS and Contentlayer',
-    author: 'CodeBucks',
-    headerTitle: 'Next.js Blog',
-    description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
-    language: 'en-us',
-    theme: 'system', // system, dark or light
-    siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
-    siteLogo: '/logo.png',
-    socialBanner: '/social-banner.png', // add social banner in the public folder
-    email: 'codebucks27@gmail.com', 
-    github: 'https://github.com/codebucks27',
-    twitter: 'https://twitter.com/code_bucks',
-    facebook: 'https://facebook.com',
-    youtube: 'https://youtube.com/codebucks',
-    linkedin: 'https://www.linkedin.com/in/codebucks/',
-    dribbble: 'https://www.dribbble.com',
-    locale: 'en-US',
-  }
-  
-  module.exports = siteMetadata
+  title: "Durgesh Gupta",
+  author: "Durgesh Gupta",
+  headerTitle: "Freelance Web Developer from Mumbai, India",
+  description:
+    "Full-stack web developer, designer and marketer from Mumbai, India",
+  language: "en-us",
+  theme: "system", // system, dark or light
+  siteUrl: "https://durgeshgupta.com", // your website URL
+  siteLogo: "/logo.png",
+  socialBanner: "/social-banner.png", // add social banner in the public folder
+  email: "imdurgeshgupta@gmail.com",
+  github: "https://github.com/ilearnbydoing",
+  twitter: "https://x.com/@ilearnbydoing",
+  facebook: "https://facebook.com",
+  youtube: "https://youtube.com/codebucks",
+  linkedin: "https://www.linkedin.com/in/ilearnbydoing/",
+  dribbble: "https://www.dribbble.com",
+  locale: "en-US",
+};
+
+module.exports = siteMetadata;
