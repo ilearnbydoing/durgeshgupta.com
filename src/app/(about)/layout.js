b153a9879acb9a6ed07f868ffc930daabc3772b5
ilearnbydoing <dgweb123@gmail.com>
2023-09-26 12:00:48 +0530

    update

--- src/app/(about)/layout.js
+++ src/app/(about)/layout.js
@@ -1,15 +1,14 @@
 import InsightRoll from "@/src/components/About/InsightRoll";
 
-
 const insights = [
-    "20+ Projects Completed",
-    "3+ Years of Freelancing",
-    "99% Client Satisfaction",
-    "20K+ Subscribers",
-    "Authored In-Depth Course on Educative",
-    "Contributed as a Technical Course Reviewer 📝",
-    "Recipient of the Hackernoon Noonies Award 🏆",
-  ];
+  "1k+ Projects Completed",
+  "15+ Years of Freelancing",
+  "99% Client Satisfaction",
+  "A Blogger",
+  "Freelance Professional",
+  "Web Consultant",
+  "Solopreneur",
+];
 
 export default function AboutLayout({ children }) {
   return (
