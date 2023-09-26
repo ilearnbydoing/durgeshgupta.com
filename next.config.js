b56f470d697ba70337886c00f2ddbc209ef69934
ilearnbydoing <dgweb123@gmail.com>
2023-09-26 11:44:01 +0530

    update

--- next.config.js
+++ next.config.js
@@ -1,11 +1,43 @@
 /** @type {import('next').NextConfig} */
 
-const {withContentlayer} = require("next-contentlayer")
+const { withContentlayer } = require("next-contentlayer");
 
 const nextConfig = {
-    compiler:{
-        removeConsole: true,
-    }
+  compiler: {
+    removeConsole: true,
+  },
+  redirects: async () => {
+    return [
+      // Add your redirects here
+      {
+        source:
+          "/unlocking-growth-your-go-to-digital-marketing-freelancer-in-india",
+        destination:
+          "/blog/unlocking-growth-your-go-to-digital-marketing-freelancer-in-india",
+        permanent: true,
+      },
+      {
+        source: "/how-to-fix-public-storage-link-already-exists-in-laravel-10",
+        destination:
+          "/blog/how-to-fix-public-storage-link-already-exists-in-laravel-10",
+        permanent: true,
+      },
+      {
+        source: "/exploring-various-approaches-to-develop-laravel-applications",
+        destination:
+          "/blog/exploring-various-approaches-to-develop-laravel-applications",
+        permanent: true,
+      },
+      {
+        source:
+          "/unlocking-growth-your-go-to-digital-marketing-freelancer-in-india",
+        destination:
+          "/blog/unlocking-growth-your-go-to-digital-marketing-freelancer-in-india",
+        permanent: true,
+      },
+      // Add more redirects as needed
+    ];
+  },
 };
 
 module.exports = withContentlayer({ ...nextConfig });
